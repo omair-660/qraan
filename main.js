396cf8344ef0dce514295a90bd2b3e76ba81fa00
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2023-10-01 04:48:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
 let scroll = document.querySelector('.up');
-let click = document.getElementById("click");
-let click1 = document.getElementById("click1");
-let click2 = document.getElementById("click2");
-let click3 = document.getElementById("click3");
-let click4 = document.getElementById("click4");
-let click5 = document.getElementById("click5");
-let click6 = document.getElementById("click6");
-let click7 = document.getElementById("click7");
-let click8 = document.getElementById("click8");
-let click9 = document.getElementById("click9");
-let click10 = document.getElementById("click10");
-let click11 = document.getElementById("click11");
-let click12 = document.getElementById("click12");
-let click13 = document.getElementById("click13");
-let click14 = document.getElementById("click14");
-let click15 = document.getElementById("click15");
-let click16 = document.getElementById("click16");
-let click17 = document.getElementById("click17");
-let click18 = document.getElementById("click18");
-let click19 = document.getElementById("click19");
-let click20 = document.getElementById("click20");
-let click21 = document.getElementById("click21");
-let click22 = document.getElementById("click22");
-let click23 = document.getElementById("click23");
-let click24 = document.getElementById("click24");
 let aside = document.getElementById("aside");
 let menu = document.getElementById("menu");
 
@@ -34,7 +9,8 @@ document.body.classList.toggle("dark-mode");
 document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
 
 menu.onclick = function () {
-  aside.classList.toggle("opeen")
+  aside.classList.toggle("opeen");
+  menu.classList.toggle("active");
 }
 
 window.onscroll = function(){
@@ -47,274 +23,260 @@ else {
 
 }
 }
+scroll.onclick = function () {
+  window.scrollTo({
+    top:0,
 
-let start = 0;
-let max = 3;
-
- click.onclick = function() {
-   if (start != 3) {
-     start++;
-
-   }else {
-     max = 0
-   }
-
-  click.innerHTML = start;
+  })
 }
-click1.onclick = function() {
-  if (start != 3) {
-    start++;
+let input = document.getElementById("input");
+let input2 = document.getElementById("input2");
+let input3 = document.getElementById("input3");
+let input4 = document.getElementById("input4");
+let input5 = document.getElementById("input5");
+let input6 = document.getElementById("input6");
+let input7 = document.getElementById("input7");
+let input8 = document.getElementById("input8");
+let input9 = document.getElementById("input9");
+let input10 = document.getElementById("input10");
+let input11 = document.getElementById("input11");
+let input12 = document.getElementById("input12");
 
-  }else {
-    max = 0
+let i = 1;
+let i1 = 1;
+let i2 = 1;
+let i3 = 1;
+let i4 = 1;
+let i5 = 1;
+let i6 = 1;
+let i7 = 1;
+let i8 = 1;
+let i9 = 1;
+let i10 = 1;
+let i11 = 1;
+
+function three(id) {
+
+  if (i <= 3) {
+    input.value = `${i++} مرة`;
+
   }
-
- click1.innerHTML = start;
 }
-let start1 = 0;
-let max1 = 4;
 
- click2.onclick = function() {
-   if (start1 != 4) {
-     start1++;
+function three1(id) {
 
-   }else {
-     max1 = 0
-   }
+  if (i1 <= 3) {
+    input2.value = `${i1++} مرة`;
 
-  click2.innerHTML = start1;
-}
-click3.onclick = function() {
-  if (start != 3) {
-    start++;
-
-  }else {
-    max = 0
   }
-
- click3.innerHTML = start;
 }
-click4.onclick = function() {
-  if (start != 3) {
-    start++;
 
-  }else {
-    max = 0
+function three2(id) {
+
+  if (i2 <= 3) {
+    input3.value = `${i2++} مرة`;
+
   }
-
- click4.innerHTML = start;
 }
-let start2 = 0;
-let max2 = 100;
 
- click5.onclick = function() {
-   if (start2 != 100) {
-     start2++;
+function three3(id) {
 
-   }else {
-     max2 = ''
-   }
+  if (i3 <= 3) {
+    input4.value = `${i3++} مرة`;
 
-  click5.innerHTML = start2;
-}
-let start3 = 0;
-let max3 = 10;
-
- click6.onclick = function() {
-   if (start3 != 10) {
-     start3++;
-
-   }else {
-     max3 = ''
-   }
-
-  click6.innerHTML = start3;
-}
-click7.onclick = function() {
-  if (start2 != 100) {
-    start2++;
-
-  }else {
-    max2 = ''
   }
-
- click7.innerHTML = start2;
 }
-click8.onclick = function() {
-  if (start != 3) {
-    start++;
+function three4(id) {
 
-  }else {
-    max = 0
+  if (i4 <= 3) {
+    input5.value = `${i4++} مرة`;
+
   }
-
- click8.innerHTML = start;
-}
-click9.onclick = function() {
-  if (start2 != 100) {
-    start2++;
-
-  }else {
-    max2 = ''
-  }
-
- click9.innerHTML = start2;
-}
-click10.onclick = function() {
-  if (start3 != 10) {
-    start3++;
-
-  }else {
-    max3 = ''
-  }
-
- click10.innerHTML = start3;
-}
-click11.onclick = function() {
-  if (start != 3) {
-    start++;
-
-  }else {
-    max = 0
-  }
-
- click11.innerHTML = start;
-}
-click12.onclick = function() {
-  if (start1 != 4) {
-    start1++;
-
-  }else {
-    max1 = 0
-  }
-
- click12.innerHTML = start1;
-}
-click13.onclick = function() {
-  if (start != 3) {
-    start++;
-
-  }else {
-    max = 0
-  }
-
- click13.innerHTML = start;
 }
 
-let start4 = 0;
-let max4 = 7;
+function three5(id) {
 
-click14.onclick = function() {
-  if (start4 != 7) {
-    start4++;
+  if (i5 <= 3) {
+    input6.value = `${i5++} مرة`;
 
-  }else {
-    max4 = 0
   }
-
- click14.innerHTML = start4;
-}
-click15.onclick = function() {
-  if (start != 3) {
-    start++;
-
-  }else {
-    max = 0
-  }
-
- click15.innerHTML = start;
-}
-click16.onclick = function() {
-  if (start != 3) {
-    start++;
-
-  }else {
-    max = 0
-  }
-
- click16.innerHTML = start;
-}
-click17.onclick = function() {
-  if (start2 != 100) {
-    start2++;
-
-  }else {
-    max2 = ''
-  }
-
- click17.innerHTML = start2;
 }
 
-click18.onclick = function() {
-  if (start3 != 10) {
-    start3++;
+function three6(id) {
 
-  }else {
-    max3 = ''
+  if (i6 <= 3) {
+    input7.value = `${i6++} مرة`;
+
   }
-
- click18.innerHTML = start3;
 }
 
-click19.onclick = function() {
-  if (start2 != 100) {
-    start2++;
+function three7(id) {
 
-  }else {
-    max2 = ''
+  if (i8 <= 3) {
+    input8.value = `${i7++} مرة`;
+
   }
-
- click19.innerHTML = start2;
 }
-click21.onclick = function() {
-  if (start3 != 10) {
-    start3++;
 
-  }else {
-    max3 = ''
+function three8(id) {
+
+  if (i8 <= 3) {
+    input9.value = `${i8++} مرة`;
+
   }
-
- click21.innerHTML = start3;
 }
-click20.onclick = function() {
-  if (start != 3) {
-    start++;
 
-  }else {
-    max = '';
+function three9(id) {
+
+  if (i9 <= 3) {
+    input10.value = `${i9++} مرة`;
+
   }
-
- click20.innerHTML = start;
 }
-click22.onclick = function() {
-  if (start != 3) {
-    start++;
 
-  }else {
-    max = ''
+function three10(id) {
+
+  if (i10 <= 3) {
+    input11.value = `${i10++} مرة`;
+
   }
-
- click22.innerHTML = start;
 }
-click23.onclick = function() {
-  if (start != 3) {
-    start++;
 
-  }else {
-    max = '';
+function three11(id) {
+
+  if (i11 <= 3) {
+    input12.value = `${i11++} مرة`;
+
   }
-
- click23.innerHTML = start;
 }
-let start5 = 0;
-let max5 = 33;
-click24.onclick = function() {
-  if (start5 != 33) {
-    start5++;
 
-  }else {
-    max5 = '';
+let input13 = document.getElementById("input13");
+let input14 = document.getElementById("input14");
+let input15 = document.getElementById("input15");
+let input16 = document.getElementById("input16");
+let input17 = document.getElementById("input17");
+let input18 = document.getElementById("input18");
+let input19 = document.getElementById("input19");
+let input20 = document.getElementById("input20");
+let input21 = document.getElementById("input21");
+let input22 = document.getElementById("input22");
+let input23 = document.getElementById("input23");
+let input24 = document.getElementById("input24");
+let input25 = document.getElementById("input25");
+
+let i12 = 1;
+let i13 = 1;
+let i14 = 1;
+let i15 = 1;
+let i16 = 1;
+let i17 = 1;
+let i18 = 1;
+let i19 = 1;
+let i20 = 1;
+let i21 = 1;
+let i22 = 1;
+let i23 = 1;
+let i24 = 1;
+
+function three12(id) {
+
+  if (i12 <= 4) {
+    input13.value = `${i12++} مرة`;
+
   }
+}
 
- click24.innerHTML = start5;
+function three13(id) {
+
+  if (i13 <= 4) {
+    input14.value = `${i13++} مرة`;
+
+  }
+}
+
+function three14(id) {
+
+  if (i14 <= 7) {
+    input15.value = `${i14++} مرة`;
+
+  }
+}
+
+function three15(id) {
+
+  if (i15 <= 7) {
+    input16.value = `${i15++} مرة`;
+
+  }
+}
+
+function hun1(id) {
+
+  if (i16 <= 100) {
+    input17.value = `${i16++} مرة`;
+
+  }
+}
+
+function hun2(id) {
+
+  if (i17 <= 100) {
+    input18.value = `${i17++} مرة`;
+
+  }
+}
+function hun3(id) {
+
+  if (i18 <= 100) {
+    input19.value = `${i18++} مرة`;
+
+  }
+}
+
+function hun4(id) {
+
+  if (i19 <= 100) {
+    input20.value = `${i19++} مرة`;
+
+  }
+}
+
+function hun5(id) {
+
+  if (i20 <= 100) {
+    input21.value = `${i20++} مرة`;
+
+  }
+}
+
+function ten1(id) {
+
+  if (i21 <= 10) {
+    input22.value = `${i21++} مرة`;
+
+  }
+}
+
+function ten2(id) {
+
+  if (i22 <= 10) {
+    input23.value = `${i22++} مرة`;
+
+  }
+}
+
+function ten3(id) {
+
+  if (i23 <= 10) {
+    input24.value = `${i23++} مرة`;
+
+  }
+}
+
+function ten4(id) {
+
+  if (i24 <= 10) {
+    input25.value = `${i24++} مرة`;
+
+  }
 }
